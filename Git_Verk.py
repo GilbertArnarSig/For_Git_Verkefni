d8d97ecac51e39db2effdc8d97621b4e6561755c
1308952559 <gilbertsi255@nemi.tskoli.is>
2017-01-25 14:36:56 +0000

    Lidur 1 buinn

---
+++ Git_Verk.py
@@ -0,0 +1,15 @@
+# Gilbert Arnar Sigurðsson
+# 25.01.17
+# Git verkefni
+
+# Dæmi 1
+#Hér fyrir neðan er forrit sem tekkur tvær tölur frá notanda og legur þær saman og margfaldar þær
+
+tala = int(input("Veldu tölu ")) # Byður notanda um tölu
+tala2 = int(input("Veldu aðra tölu "))# Byður notanda um aðra tölu
+samanlagt = tala + tala2 # legur saman tölu1 og tölu2
+margfoldun = tala * tala2 # margfaldar tölu1 og tölu2 saman
+
+print(tala,"+",tala2,"=",samanlagt) # byrtir reikning fyrir samanlagningu
+print(tala,"*",tala2,"=",margfoldun)# byrtir margföldunar reikningiginn
+print("") # noitað fyrir bil
